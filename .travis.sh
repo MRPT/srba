716698067a17df04ebfb1834665459edc1d02e9a
Jose-Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2015-11-05 00:02:08 +0100

    Generate dox from travis

--- .travis.sh
+++ .travis.sh
@@ -21,7 +21,51 @@ function test ()
   make test
 }
 
+function build_docs ()
+{
+# Do not build docs twice, once for each compiler!!
+if [ "$CC" != "gcc" ]; then
+  return
+fi
+
+# clear and re-create the out directory
+OUT_DIR=$SRC_DIR/docs/html
+
+rm -rf $OUT_DIR || exit 0;
+mkdir -p $OUT_DIR;
+
+cd $SRC_DIR/docs/
+
+# Get MRPT doxygen tags, so MRPT classes link to their docs:
+wget -q -O dox_mrpt.tag http://reference.mrpt.org/devel/dox_mrpt.tag
+
+# Run doxygen
+doxygen
+
+cd $OUT_DIR
+git init
+
+# inside this git repo we'll pretend to be a new user
+git config user.name "Travis CI"
+git config user.email "joseluisblancoc@gmail.com"
+
+# The first and only commit to this new Git repo contains all the
+# files present with the commit message "Deploy to GitHub Pages".
+git add .
+git commit -m "Deploy to GitHub Pages"
+
+# Force push from the current repo's master branch to the remote
+# repo's gh-pages branch. (All previous history on the gh-pages branch
+# will be lost, since we are overwriting it.) We redirect any output to
+# /dev/null to hide any sensitive credential data that might otherwise be exposed.
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+
+}
+
+
 case $TASK in
   build ) build;;
   test ) test;;
 esac
+
+
---
+++ docs/mainpage_dox.h
@@ -0,0 +1,23 @@
+/** \mainpage SRBA
+
+# Description
+
+Sparser Relative Bundle Adjustment (SRBA): a C++ framework for relative SLAM: 
+
+See https://github.com/MRPT/srba for papers and further theoretical information.
+
+# API reference
+
+The main classes are:
+
+- srba::RbaEngine
+
+C++ Examples: 
+
+- See demos under https://github.com/MRPT/srba/tree/master/examples/cpp
+
+ 
+*/
+
+
+
